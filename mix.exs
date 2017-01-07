444678e55a0390b6ddc858c23fe8e670521cdb14
polmuz <pablomouzo@gmail.com>
2017-01-07 15:22:52 -0300

    Version 1.0 release

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Geohash.Mixfile do
 
   def project do
     [app: :geohash,
-     version: "0.1.1",
+     version: "1.0.0",
      elixir: "~> 1.1",
      description: description(),
      package: package(),
